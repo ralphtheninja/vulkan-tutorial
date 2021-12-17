1606aab70de7b067b56d86197ba5d01ebfa05c7a
Lars-Magnus Skog <ralphtheninja@riseup.net>
2021-12-17 17:11:07 +0100

    Add presentFamily option (should fail now)

--- main.cpp
+++ main.cpp
@@ -52,9 +52,10 @@ void DestroyDebugUtilsMessengerEXT (VkInstance instance, VkDebugUtilsMessengerEX
 
 struct QueueFamilyIndices {
   std::optional<uint32_t> graphicsFamily;
+  std::optional<uint32_t> presentFamily;
 
   bool isComplete () {
-    return graphicsFamily.has_value();
+    return graphicsFamily.has_value() && presentFamily.has_value();
   }
 };
 

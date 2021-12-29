a1d909e7ae1ea0d1baefb1d9a69bf52ff0f31ac1
Lars-Magnus Skog <ralphtheninja@riseup.net>
2021-12-29 22:16:11 +0100

    Add another vertices set for two triangles

--- main.cpp
+++ main.cpp
@@ -88,6 +88,16 @@ const std::vector<Vertex> vertices = {
   {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
 };
 
+// Two triangles
+// const std::vector<Vertex> vertices = {
+//   {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+//   {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+//   {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+//   {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+//   {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
+//   {{-0.75f, -0.5f}, {1.0f, 0.0f, 1.0f}},
+// };
+
 /**
  * For reading in shaders
  */
